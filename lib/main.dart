c2a71be9d3c72a35c9e59226f02a5404bdf27d6f
Thayane <thayanebitencourt.ssa@gmail.com>
2022-02-10 21:43:50 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart' ; 
+
+void main () { 
+  runApp ( const MeuAplicativo ()); }  
+
+
+class MyApp extends StatelessWidget { const MyApp ({ Key ? key }) : super ( key : key );    
+     
+
+  @sobrepor
+  Widget build ( BuildContext context ) { return MaterialApp ( 
+      title : 'Welcome to Flutter' , 
+      home : Scaffold ( 
+        appBar : AppBar ( 
+          title : const Text ( 'Welcome to Flutter' ), ), 
+        body : const Center ( 
+          child : Text ( ' Olá Mundo' ), ), ), ); } } 
+          
+           
+        import 'package:flutter/material.dart' ; 
+
+void main () { 
+  runApp ( const MeuAplicativo ()); }  
+
+
+class MyApp extends StatelessWidget { const MyApp ({ Key ? key }) : super ( key : key );    
+     
+
+  @sobrepor
+  Widget build ( BuildContext context ) { return MaterialApp ( 
+      title : 'Welcome to Flutter' , 
+      home : Scaffold ( 
+        appBar : AppBar ( 
+          title : const Text ( 'Welcome to Flutter' ), ), 
+        body : const Center ( 
+          child : Text ( ' Olá Mundo' ), ), ), ); } } 
+          
+           
+        
